43c5fb857ba8dd1301eed6dfda681ed4f0a8e221
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-05 12:23:55 +0300

    add boilerplate code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+      <Home />
     </div>
   );
 }
