124f938b901f3dbcee8e54c99c198f165fd82ee6
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-05 22:57:34 +0300

    use Switch instead of Routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import Cards from "./components/Cards";
+// import DestinationDetails from "./components/pages/DestinationDetails";
+import Login from "./components/Login";
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
-  );
+    <>
+        <Router>
+          <Navbar/>
+          <Switch>
+            <Route exact path="/" component = {Home} ></Route>
+            <Route path="/about" exact component = {About} ></Route>
+            <Route path="/dog-page" exact component = {Cards} ></Route>
+            <Route path="/login" exact component = {<Login/>} ></Route>
+          </Switch>
+          <Footer />
+        </Router>
+    </>
+  )
 }
 
 export default App;
