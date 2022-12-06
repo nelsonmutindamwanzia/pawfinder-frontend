ee298ca91c4b85502ac15e3abae1cae0f8fd1dd7
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-06 11:04:00 +0300

    update button path to /login

--- src/components/Button.js
+++ src/components/Button.js
@@ -18,7 +18,7 @@ export const Button = ({
     const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0]
 
     return (
-        <Link to = "/add-missing-pet" className = "btn-mobile">
+        <Link to = "/login" className = "btn-mobile">
             <button
             className = {`${checkButtonStyle} ${checkButtonSize}`}
             onClick = {onClick}
