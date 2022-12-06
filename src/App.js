1384ff335126189009a9840fb4b4759d8eabf224
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-06 16:50:52 +0300

    update /about and /contacts routes

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Home from "./components/pages/Home";
-import About from "./components/pages/About";
-import Cards from "./components/Cards";
+// import Cards from "./components/Cards";
 // import DestinationDetails from "./components/pages/DestinationDetails";
 import Login from "./components/Login";
 import Footer from './components/Footer';
 import AddMissingPet from './components/pages/AddMissingPet';
+import AboutContact from './components/pages/AboutContact';
 
 
 function App() {
@@ -18,8 +18,8 @@ function App() {
           <Navbar/>
           <Switch>
             <Route exact path="/" component = {Home} ></Route>
-            <Route path="/about" exact component = {About} ></Route>
-            <Route path="/dog-page" exact component = {Cards} ></Route>
+            <Route path="/about" exact component = {AboutContact} ></Route>
+            <Route path="/contacts" exact component = {AboutContact} ></Route>
             <Route path="/add-missing-pet" exact component = {AddMissingPet} ></Route>
             <Route path="/login" exact component = {Login} ></Route>
           </Switch>
