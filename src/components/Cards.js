d9933a2d617cc460cdad4d65f85f75c8d0d61d86
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-06 14:23:43 +0300

    update dog card and styles

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -23,17 +23,21 @@ function handleClick (index){
 
 return (
   <>
+  <div id="pet-list">
+    <h1>Missing Pets</h1>
+  </div>
     {dogs.length && (
       <React.Fragment>
-      <Card className = "destination-card" body>
-      <h1>Take a look at these incredible destinations: </h1>
+      <Card className = "dog-card" body>
+      <h1>Have you seen me ? </h1>
         <div>
-          <div className="destination-info">
-          <img src={dogs[currentIndex].src} alt="destination"/>
+          <div className="dog-info">
+          <img src={dogs[currentIndex].image_url} alt={dogs[currentIndex].name}/>
             <div>
-              <h4 className="bold-text">{dogs[currentIndex].label}</h4>
+              <h4 className="bold-text"><b>Name: </b>{dogs[currentIndex].name}</h4>
+              <h4 className="bold-text"><b>Breed:</b> {dogs[currentIndex].breed}</h4>
             </div>
-            <p className="p-text">{dogs[currentIndex].last_seen}</p>
+            <p className="p-text"><b>Last Seen: </b> {dogs[currentIndex].last_seen}</p>
           </div>
         </div>
         <div className="center-btn">
