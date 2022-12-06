2d25d13ac87f3962a67102ff68817a511cd57320
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-06 11:03:10 +0300

    update hero section colors, images and add button navigation

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -2,14 +2,26 @@ import './HeroSection.css';
 
 function HeroSection() {
 return (
-    <>
-    <div className="hero-section">
-        <div className="cta">
-            <h1>Find Your Missing Pet</h1>
-            <p>Are you sick of searching for your pets all over the neighbourhood and making repetitive posts online? This app is for you.</p>
+    <div className="hero-section-container">
+        <div className="hero-section-text">
+            <h2>Find your Missing Pet</h2>
+            <p>
+                Are you sick of searching for your pets all over the
+            <br></br>
+                neighbourhood and making repetitive posts online? 
+            <br></br>
+                This app is for you!
+            </p>
+
+        <a href="#pet-list" className="view-btn">
+            View Missing Pets
+        </a>
         </div>
+        <div className="hero-section-image">
+            <img src="https://iili.io/HCS8i37.png" alt="hero-paw" />
+        </div>
+        
     </div>
-    </>
-)
+);
 }
 export default HeroSection
