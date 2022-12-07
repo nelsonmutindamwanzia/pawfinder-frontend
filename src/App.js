9066e02db57f5c337adf1b501a655836ba659847
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-07 20:02:17 +0300

    refactor routing, set login and signup pages to be first viewed page if user is logged out, add auto-login feature upon signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Home from "./components/pages/Home";
-// import Cards from "./components/Cards";
-// import DestinationDetails from "./components/pages/DestinationDetails";
+// import Button from "./components/Button";
 import Signup from './components/Signup'
 import Login from "./components/Login";
 import Footer from './components/Footer';
@@ -13,17 +12,38 @@ import AboutContact from './components/pages/AboutContact';
 
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    // auto-login
+    fetch("/me").then((r) => {
+      if (r.ok) {
+        r.json().then((user) => setUser(user));
+      }
+    });
+  }, []);
+
+  if (!user) return (
+      <>
+      <Router>
+        <Switch>
+          <Route><Login onLogin={setUser} signup = {<Signup onLogin={setUser}/>} /></Route>
+        </Switch>
+      </Router>
+      </>
+  );
+
   return (
     <>
         <Router>
-          <Navbar/>
+          <Navbar user={user} setUser={setUser}/>
           <Switch>
-            <Route exact path="/" component = {Home} ></Route>
+            <Route exact path="/" component = {Home} user={user} ></Route>
             <Route path="/about" exact component = {AboutContact} ></Route>
             <Route path="/contacts" exact component = {AboutContact} ></Route>
             <Route path="/add-missing-pet" exact component = {AddMissingPet} ></Route>
-            <Route path="/login" exact component = {Login} ></Route>
-            <Route path="/signup" exact component = {Signup} ></Route>
+            <Route path="/login" exact component = {Login} setUser = {setUser}></Route>
+            <Route path="/signup" exact component = {Signup} setUser = {setUser} ></Route>
           </Switch>
           <Footer />
         </Router>
