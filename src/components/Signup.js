229f31adce26e47e11040fabe6874aa859c4f0a0
nelsonmutindamwanzia <nmutinda02@gmail.com>
2022-12-07 19:35:19 +0300

    update signup form and styling

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import './Signup.css';
+import styled from "styled-components";
+import Error from './Error';
 
 function Signup({ onLogin }) {
 
@@ -6,6 +9,12 @@ function Signup({ onLogin }) {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [errors, setErrors] = useState([]);
+
+    const FormField = styled.div`
+        &:not(:last-child) {
+        margin-bottom: 12px;
+    }`;
 
     function handleSubmit(e) {
         e.preventDefault();
@@ -20,41 +29,60 @@ function Signup({ onLogin }) {
             password,
             password_confirmation: passwordConfirmation,
         }),
-        })
-        .then((r) => r.json())
-        .then(onLogin);
+    }).then((r) => {
+        if (r.ok) {
+            r.json().then((user) => onLogin(user));
+        } else {
+            r.json().then((err) => setErrors(err.errors));
+        }
+    });
     }
     return (
         <form onSubmit={handleSubmit} className = "signup-form">
-        <label htmlFor="username" className="label">Username:</label>
+        <h1>🐾 PawFinder</h1>
+        <h1>  Signup Form</h1>
+        <label htmlFor="username">Username:</label>
         <input
             type="text"
+            autoComplete="off"
             id="username"
+            placeholder="  Enter your username..."
             value={username}
             onChange={(e) => setUsername(e.target.value)}
         />
-        <label htmlFor="email" className="label">Email:</label>
+        <label htmlFor="email">Email:</label>
         <input
             type="email"
             id="email"
+            autoComplete="off"
+            placeholder="  Enter your email address..."
             value={email}
             onChange={(e) => setEmail(e.target.value)}
         />
-        <label htmlFor="password" className="label">Password:</label>
+        <label htmlFor="password">Password:</label>
         <input
             type="password"
             id="password"
+            placeholder="  Enter your password..."
             value={password}
+            autoComplete="off"
             onChange={(e) => setPassword(e.target.value)}
         />
-        <label htmlFor="password_confirmation" className="label">Confirm Password:</label>
+        <label htmlFor="password_confirmation">Confirm Password:</label>
         <input
             type="password"
             id="password_confirmation"
+            placeholder="  Re-enter your password..."
+            autoComplete="off"
             value={passwordConfirmation}
             onChange={(e) => setPasswordConfirmation(e.target.value)}
         />
-        <button type="submit" className = "button2">Submit</button>
+        <button type="submit">Sign Up</button>
+        <FormField>
+            {errors.map((err) => (
+            <Error key={err}>{err}</Error>
+            ))}
+        </FormField>
         </form>
     );
 }
