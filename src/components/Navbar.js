80962a5d8a28d0b4bf7dcf203b9134f1863e7002
nelsonmutindamwanzia <nmutinda02@gmail.com>
2023-09-05 11:56:16 +0300

    update jsx to prevent the logout button from overlapping logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,29 @@
-import React, {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import Button from './Button';
 import './Navbar.css';
 
-function Navbar({user, setUser}) {
+function Navbar({ user, setUser }) {
     const [click, setClick] = useState(false);
     const [button, setButton] = useState(true);
 
-    function handleClick(){
+    function handleClick() {
         setClick(!click);
     }
-    function closeMobileMenu(){
+
+    function closeMobileMenu() {
         setClick(false);
     }
 
-    function showButton (){
-        if (window.innerWidth <= 960){
+    function showButton() {
+        if (window.innerWidth <= 960) {
             setButton(false);
         } else {
             setButton(true);
         }
     }
 
-    useEffect(()=>{
+    useEffect(() => {
         showButton();
     }, []);
 
@@ -30,39 +31,41 @@ function Navbar({user, setUser}) {
 
     return (
         <>
-            <div className ="navbar">
-                <div className ="navbar-container">
-                    <Link to='/' className='navbar-logo' onClick = {closeMobileMenu}>
-                    🐾 PawFinder
-                        <i class="fas fa-paw" />
+            <div className="navbar">
+                <div className="navbar-container">
+                    <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
+                        🐾 PawFinder
+                        <i className="fas fa-paw" />
                     </Link>
-                    <div className = "menu-icon" onClick = {handleClick}>
-                        <i className = {click ? "fas fa-times":"fas fa-bars"} />
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? "fas fa-times" : "fas fa-bars"} />
                     </div>
-                    <ul className = {click ? "nav-menu active":"nav-menu"}>
-                        <li className = "nav-item">
-                            <Link to = "/about" className = "nav-links" onClick = {closeMobileMenu}>
-                                About
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/contacts" className = "nav-links" onClick = {closeMobileMenu}>
-                                Contacts
-                            </Link>
-                        </li>
-                        <li className = "nav-item">
-                            <Link to = "/add-missing-pet" className = "nav-links" onClick = {closeMobileMenu}>
-                                + Add Missing Pet
-                            </Link>
-                        </li>
-                    </ul>
-                    <div className="btn-logout">
-                        {<Button user={user} setUser={setUser}/>}
-                    </div>
+                    {button ? (
+                        <ul className={click ? "nav-menu active" : "nav-menu"}>
+                            <li className="nav-item">
+                                <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                                    About
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/contacts" className="nav-links" onClick={closeMobileMenu}>
+                                    Contacts
+                                </Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/add-missing-pet" className="nav-links" onClick={closeMobileMenu}>
+                                    + Add Missing Pet
+                                </Link>
+                            </li>
+                        </ul>
+                    ) : null}
+                </div>
+                <div className="btn-logout">
+                        {<Button user={user} setUser={setUser} />}
                 </div>
             </div>
         </>
-    )
+    );
 }
 
-export default Navbar
+export default Navbar;
